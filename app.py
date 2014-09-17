3e75be36fb969eba5ec95826066f2475581b6e73
Aida Piccato <aidapiccato@gmail.com>
2014-09-16 21:13:45 -0400

    Trying to display data

--- app.py
+++ app.py
@@ -3,7 +3,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html");
+    inStream = open("data_hw252.csv", 'r')                    
+    data = inStream.read()
+    inStream.close()
+    pos = 0
+    amount = data.count('(')
+    while pos <= amount:
+        a = len(data)
+        data = data.find('(')
+        data = data[: (a - 1)] + '-' + data[a:]
+        data = data[: a] + '^' + data[a + 1:]
+        pos += 1
+    data = data.replace('^', '(')
+    data = data[:126239] 
+    return render_template("home.html", data = data);
+
 
 if __name__ == "__main__":
     app.debug = True
